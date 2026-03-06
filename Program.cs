24213bee847ae0affebc8f1f8c0aa0bdcf37305f
skliksklok <skliksklok@mail.ru>
2026-03-06 17:38:57 +0300

    delete solve task2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@
             Console.WriteLine("Hello, World!");
         }
         // Вовчук Евгений Константинович Ам-1-25
-        //solve task2
     }
     static public double Sqr(double X);
 }
